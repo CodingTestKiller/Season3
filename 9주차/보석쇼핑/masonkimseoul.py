bdbb3fef258f5fdeaac92766e9bc3a5d00152d28
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2023-10-11 19:42:41 +0900

    보석쇼핑 / 1h 20m

---
+++ 9주차/보석쇼핑/masonkimseoul.py
@@ -0,0 +1,31 @@
+def solution(gems):
+    types = set(gems)
+    if len(types) == 1:
+        return [1, 1]
+    dic = {}
+
+    for t in types:
+        dic[t] = 0
+
+    s, e = 0, 0
+    cnt = 0
+    ans = []
+    while e < len(gems) and s < len(gems):
+        if cnt < len(types):
+            if dic[gems[e]] == 0:
+                cnt += 1
+            dic[gems[e]] += 1
+            if cnt < len(types):
+                e += 1
+        else:
+            dic[gems[s]] -= 1
+            if dic[gems[s]] == 0:
+                cnt -= 1
+                ans.append((s, e, e - s + 1))
+            s += 1
+
+    ans.sort(key=lambda x: [x[2]])
+
+    return [ans[0][0] + 1, ans[0][1] + 1]
+
+print(solution(["A", "B", "B","B", "C", "D", "D", "D", "D", "D", "D", "D", "B", "C", "A"]))
